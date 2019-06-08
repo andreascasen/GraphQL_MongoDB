e172dab10eae15e64e19aa6e99844a956e18b747
Andreas <andreas.casenarg@gmail.com>
2019-06-08 13:36:13 +0200

    Added mongoconnector

--- src/devenv.js
+++ src/devenv.js
@@ -4,7 +4,7 @@
 const DB_USER = process.env.DB_USER || 'dbadmin'
 const DB_PWD = process.env.DB_PWD || 'admin123'
 export const DB_NAME = process.env.DB_NAME || 'toll-calculator'
-export const DB_URI = process.env.DB_URI || `mongodb://${ DB_USER }:${ DB_PWD }@ds149606.mlab.com:49606/${ DB_NAME }`
+export const DB_URI = `mongodb://${ DB_USER }:${ DB_PWD }@ds149606.mlab.com:49606/${ DB_NAME }`
 
 /**
  * General
